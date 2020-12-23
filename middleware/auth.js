e2fd3f134462a44682c3d637a64b4f19411f66fc
Lucas Rodríguez <lucasrdz994@gmail.com>
2020-12-23 18:35:25 -0300

    add auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,12 @@ import netlifyIdentity from 'netlify-identity-widget'
 
 export default function({redirect, store, route}) {
 
-  // if (!netlifyIdentity.currentUser() && route.path !== '/')  {
-  //   return redirect('/')
-  // }
+  if (!netlifyIdentity.currentUser() && route.path !== '/')  {
+    return redirect('/')
+  }
 
-  // if (!store.state.currentUser) {
-  //   return store.commit('setCurrentUser', netlifyIdentity.currentUser())
-  // }
+  if (!store.state.currentUser) {
+    return store.commit('setCurrentUser', netlifyIdentity.currentUser())
+  }
 
 }
